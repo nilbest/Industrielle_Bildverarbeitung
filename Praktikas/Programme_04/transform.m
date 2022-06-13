3806d2010de46b9df0ed7f0837ca1ae3d369dc5e
Nils_Laptop <104395997+Moviemonster@users.noreply.github.com>
2022-06-13 20:20:01 +0200

    Aufgabe 1 fast fertig, nur noch Matix croppen

--- Praktikas/Programme_04/transform.m
+++ Praktikas/Programme_04/transform.m
@@ -1,2 +1,32 @@
-function transform(Bild,Hintergrund,x_skal,y_skal,x_ver,y_ver,rot)
+function transform(im,back,x_skal,y_skal,x_ver,y_ver,rot)
 
+tform_skal = affine2d([x_skal 0 0; 0 y_skal  0; 0 0 1]);
+
+tform_rot= affine2d([cosd(rot) -sind(rot) 0;sind(rot) cosd(rot) 0;0 0 1]);
+%tform_rot= affine2d('Rotation',rot);
+
+im_skaled=imwarp(im,tform_skal);
+
+% figure('Name','Test skale','NumberTitle','off'); 
+% imshowpair(im,im_skaled,'montage');
+% title('Original Bild & Auswahl der Pixel');
+
+im_skaled_rot=imwarp(im_skaled,tform_rot);
+
+% figure('Name','Test 2','NumberTitle','off'); 
+% imshowpair(im,im_skaled_rot,'montage');
+% title('Original Bild & Auswahl der Pixel');
+
+[x, y]=size(im_skaled_rot);
+
+back(x_ver:x_ver+x-1,y_ver:y_ver+y-1)=im_skaled_rot;
+%Back hart abschneiden bei 1024
+ 
+figure('Name','Test 3','NumberTitle','off'); 
+imshow(back,[]);
+title('Original Bild & Auswahl der Pixel');
+
+
+% origin_mid=1024/2;
+% 
+% [x,y]=transformPointsForward(tform,origin_mid,origign_mid);
--- Praktikas/Programme_04/transformMain.m
+++ Praktikas/Programme_04/transformMain.m
@@ -1,2 +1,9 @@
 im = imread("Images\object.png");
 back=zeros(1024,1024);
+
+x_skal=2;
+y_skal=2;
+x_ver=1000;
+y_ver=1000;
+rot=90;
+transform(im,back,x_skal,y_skal,x_ver,y_ver,rot);
